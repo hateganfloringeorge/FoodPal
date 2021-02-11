74638365d0138a4ff8ccd36e977193394ed721ce
Hategan Florin George <hateganfloringeorge@gmail.com>
2021-02-11 03:49:11 +0200

    Add CatalogueItemRepository

---
+++ src/FoodPal.Providers.DataAccess/Repository/CatalogueItemRepository.cs
@@ -0,0 +1,36 @@
+﻿using FoodPal.Providers.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodPal.Providers.DataAccess.Repository
+{
+    public class CatalogueItemRepository : Repository<CatalogueItem>, ICatalogueItemRepository
+    {
+
+        public CatalogueItemRepository(ProvidersContext providersContext) : base(providersContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<CatalogueItem>> GetAllWithProviderIdAsync(int providerId)
+        {
+            return await _providersContext.CatalogueItems
+                .Include(i => i.Category)
+                .Include(i => i.Catalogue)
+                .ThenInclude(c => c.Provider)
+                .Where(x => x.Id == providerId)
+                .ToListAsync();
+        }
+
+        public async Task<CatalogueItem> GetWithProviderAndCategoryByIdAsync(int catalogueItemId)
+        {
+            return await _providersContext.CatalogueItems
+               .Include(i => i.Category)
+               .Include(i => i.Catalogue)
+               .ThenInclude(c => c.Provider)
+               .SingleOrDefaultAsync(x => x.Id == catalogueItemId);
+        }
+    }
+}
---
+++ src/FoodPal.Providers.DataAccess/Repository/ICatalogueItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using FoodPal.Providers.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodPal.Providers.DataAccess.Repository
+{
+    public interface ICatalogueItemRepository : IRepository<CatalogueItem>
+    {
+        Task<IEnumerable<CatalogueItem>> GetAllWithProviderIdAsync(int providerId);
+
+        Task<CatalogueItem> GetWithProviderAndCategoryByIdAsync(int catalogueItemId);
+    }
+}
